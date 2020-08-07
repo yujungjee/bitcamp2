5d1359de3b0aa7724d04a7d8e274e8843a85bd12
yujungjee <zuzung@gmail.com>
2020-08-07 17:01:27 +0900

    0807 1701

---
+++ ex/src/d0807/AjaxTest1.java
@@ -0,0 +1,46 @@
+package d0807;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AjaxTest1
+ */
+@WebServlet("/ajaxTest1")
+public class AjaxTest1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandler(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandler(request, response);
+	}
+
+	private void doHandler(HttpServletRequest request, HttpServletResponse response)	throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		String param = (String) request.getParameter("param");
+		String age = (String) request.getParameter("age");
+		System.out.println("param = " + param);
+		System.out.println("age = " + age);
+		PrintWriter writer = response.getWriter();
+		writer.print("안녕하세요.서버입니다.");
+	}
+
+}
